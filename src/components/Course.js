5fcff4cd44250ba8c56c2044b9fc7114913bd145
Rajgupta152 <kshitijgupta782@gmail.com>
2023-12-05 21:28:47 +0530

    commit

--- src/components/Course.js
+++ src/components/Course.js
@@ -9,7 +9,7 @@ const Course = () => {
     <Box component="div" className="all-courses">
     <Box component="div" className="row">
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg" className="image"/>
+        <img src="https://www.nifafinearts.com/images/animation-sketching.jpg" className="image"/>
         {/* <Box component="div"> */}
             <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
         {/* </Box> */}
@@ -22,9 +22,9 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/3mchc.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>1 to 3 Months Certificate Hobby Course (Regular and Weekend)</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -35,9 +35,9 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/1ydfa.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>One Year Diploma in Fine Arts-Regular/Part Time-Level-3</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -48,64 +48,9 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/2ydfa.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
-        {/* </Box> */}
-        <Button
-         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-            padding: "10px 20px",
-            color: "#fff"
-         }}>Know More</Button>
-    </Box>
-    </Box>
-
-    <Box component="div" className="row">
-    <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg" className="image"/>
-        {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
-        {/* </Box> */}
-        <Button
-         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-            padding: "10px 20px",
-            color: "#fff"
-         }}>Know More</Button>
-    </Box>
-    <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
-        {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
-        {/* </Box> */}
-        <Button
-         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-            padding: "10px 20px",
-            color: "#fff"
-         }}>Know More</Button>
-    </Box>
-    <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
-        {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
-        {/* </Box> */}
-        <Button
-         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-            padding: "10px 20px",
-            color: "#fff"
-         }}>Know More</Button>
-    </Box>
-    <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
-        {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>Two years Advance Diploma in Fine Arts - Regular/Part time</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -119,9 +64,9 @@ const Course = () => {
 
     <Box component="div" className="row">
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg" className="image"/>
+        <img src="https://www.nifafinearts.com/images/entrance-prepration.jpg" className="image"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>Entrance Preparation For N.I.D</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -132,9 +77,9 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/bfa.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>B.F.A from UGC Recognised University</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -145,9 +90,9 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/ma.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>M.A in Painting from UGC Recognised University</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
@@ -158,9 +103,64 @@ const Course = () => {
          }}>Know More</Button>
     </Box>
     <Box component="div" className="col">
-        <img src="	https://www.nifafinearts.com/images/animation-sketching.jpg"/>
+        <img src="https://www.nifafinearts.com/images/entrance-prepration3.jpg"/>
         {/* <Box component="div"> */}
-            <p>6 Months Diploma in Sketching & Oil Painting - Regular/Part time</p>
+            <p>Entrance preparation for B.F.A</p>
+        {/* </Box> */}
+        <Button
+         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
+            border: "2px solid #fff",
+            borderRadius: "30px",
+            padding: "10px 20px",
+            color: "#fff"
+         }}>Know More</Button>
+    </Box>
+    </Box>
+
+    <Box component="div" className="row">
+    <Box component="div" className="col">
+        <img src="https://www.nifafinearts.com/images/kidl1.jpg" className="image"/>
+        {/* <Box component="div"> */}
+            <p>Kids 1 year Diploma (Level-1)</p>
+        {/* </Box> */}
+        <Button
+         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
+            border: "2px solid #fff",
+            borderRadius: "30px",
+            padding: "10px 20px",
+            color: "#fff"
+         }}>Know More</Button>
+    </Box>
+    <Box component="div" className="col">
+        <img src="https://www.nifafinearts.com/images/kidl2.jpg"/>
+        {/* <Box component="div"> */}
+            <p>Kids 1 year Diploma (Level-2)</p>
+        {/* </Box> */}
+        <Button
+         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
+            border: "2px solid #fff",
+            borderRadius: "30px",
+            padding: "10px 20px",
+            color: "#fff"
+         }}>Know More</Button>
+    </Box>
+    <Box component="div" className="col">
+        <img src="https://www.nifafinearts.com/images/entrance-prepration2.jpg"/>
+        {/* <Box component="div"> */}
+            <p>Entrance Preparation for NIFT</p>
+        {/* </Box> */}
+        <Button
+         sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
+            border: "2px solid #fff",
+            borderRadius: "30px",
+            padding: "10px 20px",
+            color: "#fff"
+         }}>Know More</Button>
+    </Box>
+    <Box component="div" className="col">
+        <img src="https://www.nifafinearts.com/images/hccfk.jpg"/>
+        {/* <Box component="div"> */}
+            <p>3 Months Hobby Certificate Course For Kids</p>
         {/* </Box> */}
         <Button
          sx={{background: "linear-gradient( 90deg, rgba(177, 30, 111, 1) 0%, rgba(74, 14, 192, 1) 100%)",
